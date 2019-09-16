6eba8101cfeb90a8f21a458bae39947d1fe36056
giginet <giginet.net@gmail.com>
2019-09-16 20:29:54 +0900

    Describe packages

--- Package.swift
+++ Package.swift
@@ -6,23 +6,29 @@ import PackageDescription
 let package = Package(
     name: "Barca",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Barca",
-            targets: ["Barca"]),
+            name: "BarcaKit",
+            targets: ["BarcaKit"]),
+        .executable(name: "Barca",
+                    targets: ["Barca"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/tuist/XcodeProj.git",
+                 .upToNextMinor(from: "7.0.0")),
+        .package(url: "https://github.com/kylef/Commander.git",
+                 .upToNextMinor(from: "0.9.0")),
+        .package(url: "https://github.com/dduan/TOMLDecoder.git",
+                 .upToNextMinor(from: "0.1.3")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BarcaKit",
+            dependencies: []),
         .target(
             name: "Barca",
-            dependencies: []),
+            dependencies: ["BarcaKit"]),
         .testTarget(
-            name: "BarcaTests",
-            dependencies: ["Barca"]),
+            name: "BarcaKitTests",
+            dependencies: ["BarcaKit"]),
     ]
 )
